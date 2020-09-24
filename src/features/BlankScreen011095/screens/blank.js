c92b5f9b4aeefe10bd6063dd41a3019b23f1a9b9
Crowdbotics <team@crowdbotics.com>
2020-09-24 22:31:38 +0000

    Update components

--- src/features/BlankScreen011095/screens/blank.js
+++ src/features/BlankScreen011095/screens/blank.js
@@ -36,7 +36,6 @@ export class __Blank extends React.Component {
         Press me!1
       </Button>
       <View style={this.props.themedStyle.View_3}>
-        <View style={this.props.themedStyle.View_5} />
         <View style={this.props.themedStyle.View_6} />
       </View>
       <Button
@@ -134,36 +133,6 @@ _Blank = withStyles(__Blank, theme => ({
     fontStyle: "normal",
     fontWeight: "normal",
     flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-    alignContent: "stretch",
-    borderColor: "#000000",
-    borderStyle: "solid",
-    borderWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderTopWidth: 0,
-    borderBottomWidth: 0,
-    borderRadius: 0,
-    backgroundSize: "auto"
-  },
-  View_5: {
-    marginLeft: 0,
-    marginRight: 0,
-    marginTop: 0,
-    marginBottom: 0,
-    paddingLeft: 0,
-    paddingRight: 0,
-    paddingTop: 0,
-    paddingBottom: 0,
-    overflow: "visible",
-    fontSize: 12,
-    color: "#000000",
-    backgroundColor: "#ffffff",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    flexDirection: "column",
     flexWrap: "wrap",
     justifyContent: "flex-start",
     alignItems: "stretch",
