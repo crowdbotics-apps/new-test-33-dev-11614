35f236414b9251af8788ab66e9fd0135643c3283
Crowdbotics <team@crowdbotics.com>
2020-09-24 23:57:14 +0000

    Update components

--- src/features/BlankScreen011095/screens/blank.js
+++ src/features/BlankScreen011095/screens/blank.js
@@ -90,6 +90,19 @@ export class __Blank extends React.Component {
       >
         Press me!
       </Button>
+      <Button
+        textStyle={{
+          fontSize: 12,
+          color: "#000000",
+          textAlign: "center",
+          fontWeight: "normal",
+          fontStyle: "normal"
+        }}
+        style={this.props.themedStyle.Button_16}
+        onPress={() => alert("Pressed!")}
+      >
+        Press me!
+      </Button>
     </View>
   )
 }
@@ -310,6 +323,37 @@ _Blank = withStyles(__Blank, theme => ({
     letterSpacing: 0
   },
   Button_15: {
+    width: "100%",
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#3366FF",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "center",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
+  },
+  Button_16: {
     width: "100%",
     marginLeft: 0,
     marginRight: 0,
